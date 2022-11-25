12404bf64e753ad414b095630ad06118ff72f74a
Bruno Rocha <bruno.miguel.rocha@hotmail.com>
2022-11-25 18:07:28 +0000

    fixed error in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div>
+      <div className="topnav" id="myTopnav">
+        <Link to="/home" className="active">
+          Home
+        </Link>
+        <Link to="#news">News</Link>
+        <Link to="#contact">Contact</Link>
+        <Link to="#about">About</Link>
+        <Link to="#" className="icon" onclick="myFunction()">
+          <i className="fa fa-bars"></i>
+        </Link>
+      </div>
+
+      <div>
+        <h2>Responsive Topnav Example</h2>
+        <p>Resize the browser window to see how it works.</p>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
