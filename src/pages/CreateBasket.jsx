da95a038ee03f0eb4be0061f9e15e0aa7f1a7bb5
Hugo Esteves <hugo.veigaa@gmail.com>
2022-11-29 12:34:22 +0000

    fixed bugs with chooseBasket and createBasket

--- src/pages/CreateBasket.jsx
+++ src/pages/CreateBasket.jsx
@@ -2,20 +2,30 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function CreateBasket() {
-  const [type, setType] = useState("");
-  const [market, setMarket] = useState("");
+  const [type, setType] = useState("Hope");
+  const [market, setMarket] = useState(null);
   const [products, setProducts] = useState([]);
   const [price, setPrice] = useState(0);
   const [data, setData] = useState([]);
+  const [marketData, setMarketData] = useState([]);
 
   const handleType = (e) => setType(e.target.value);
   const handleMarket = (e) => setMarket(e.target.value);
   //const handlePrice = (e) => setPrice(e.target.value);
   const handleProducts = (e) => {
-    let sumProducts = data.reduce((acc, cur) => acc + cur.price, 0);
+    let newProducts = [...products, e.target.value];
+    setProducts(newProducts);
 
+    console.log(products);
+
+    let sumProducts = data.reduce((acc, cur) => {
+      if (newProducts.includes(cur._id)) {
+        return acc + cur.price;
+      }
+      return acc;
+    }, 0);
+    console.log(sumProducts);
     setPrice(sumProducts);
-    setProducts(e.target.value);
   };
 
   const handleSubmit = async (e) => {
@@ -25,18 +35,20 @@ function CreateBasket() {
       // getting the authorization token of the user in the local storage
       const storedToken = localStorage.getItem("authToken");
 
-      await axios.post(`${process.env.REACT_APP_API_URL}/basket`, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-        type,
-        market,
-        products,
-        price,
-      });
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}/basket`,
+        { basketType: type, market, products, price },
+        {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        }
+      );
 
-      setType("");
-      setMarket("");
+      setType("Hope");
+      setMarket(null);
       setProducts([]);
       setPrice(0);
+
+      //navigate
     } catch (error) {
       console.log(error);
     }
@@ -59,18 +71,36 @@ function CreateBasket() {
     }
   };
 
+  const getMarkets = async () => {
+    try {
+      const storedToken = localStorage.getItem("authToken");
+
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/markets`,
+        {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        }
+      );
+
+      setMarketData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getProducts();
+    getMarkets();
   }, []);
 
   return (
     <div>
-    <h1>dgsfdh</h1>
-    <h1>dgsfdh</h1>
-    <h1>dgsfdh</h1>
-    <h1>dgsfdh</h1>
+      <h1>dgsfdh</h1>
+      <h1>dgsfdh</h1>
+      <h1>dgsfdh</h1>
+      <h1>dgsfdh</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="userType">What kind of help do you wish to give?</label>
+        {/* <label htmlFor="userType">What kind of help do you wish to give?</label>
 
         <select
           name="basketType"
@@ -84,16 +114,32 @@ function CreateBasket() {
           <option value="M">M</option>
           <option value="L">L</option>
           <option value="Hope">Hope</option>
-        </select>
+        </select> */}
 
         <label htmlFor="market">Where are you dropping your basket?</label>
-        <input
+
+        <select
+          name="market"
+          id="market"
+          required
+          size="5"
+          onChange={handleMarket}
+        >
+          {marketData.map((market) => (
+            <option key={market._id} value={market._id}>
+              {market.name}
+            </option>
+          ))}
+        </select>
+
+        {/*  <input
           type="text"
           name="market"
           value={market}
           onChange={handleMarket}
-        />
+        /> */}
 
+        <label htmlFor="produt">What are you offering?</label>
         {data.map((product) => (
           <div key={product._id}>
             <img src={product.imageUrl} alt="product choice" />
@@ -101,7 +147,7 @@ function CreateBasket() {
             <input
               type="radio"
               label={product}
-              value={product}
+              value={product._id}
               name={product._id}
               onClick={handleProducts}
             />
